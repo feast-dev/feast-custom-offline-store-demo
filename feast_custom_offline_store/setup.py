dcd2f07d75fa9534c9e4728f4d2e7a61f4167ef8
Felix Wang <wangfelix98@gmail.com>
2021-10-23 18:27:12 -0700

    Update setup.py and requirements.txt
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

--- feast_custom_offline_store/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="feast_custom_offline_store",
-    version="0.0.1",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    install_requires=["feast==0.12.1"],
-)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+NAME = "feast_custom_stores_demo"
+REQUIRES_PYTHON = ">=3.7.0"
+
+setup(
+    name=NAME,
+    version="0.0.1",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(include=["feast_custom_offline_store"]),
+    install_requires=["feast==0.12.1"],
+    license="Apache",
+)
